fface3b9f9f7b6f52fcceab47cfb933761bd6f9a
Cahian Freire <cahianfreire@gmail.com>
2023-06-26 23:01:05 -0300

    Update code

--- source/main.py
+++ source/main.py
@@ -1,5 +1,21 @@
+from os import getenv
+from pprint import pprint
+
+import openai
+
+
+openai.my_api_key = getenv("OPENAI_API_KEY")
+completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hello!"}
+    ]
+)
+
+
 def main():
-    pass
+    pprint(completion)
 
 
 if __name__ == "__main__":
